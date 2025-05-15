83be15aec64bd81c16d21550787c726250dcf947
zc <zhangchanget@126.com>
2025-05-15 15:38:21 +0800

    2025.5.15第一次上传

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2025/05/15 15:36
+# @Author  : zc
+# @File    : test.py
+
+print("123456")
+print("123456")
+print("123456")
+print("123456")
+print("123456")
+print("123456")
